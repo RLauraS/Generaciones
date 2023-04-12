2d4a850c642dffd9db342fe52e0739376c0948e4
RLauraS <R_LauraS@yahoo.com.ar>
2023-04-12 04:46:19 -0300

    Se finalizan las funciones crear persona y mostrar generacion

--- js/app.js
+++ js/app.js
@@ -12,23 +12,35 @@ let formulario = document.querySelector("form");
 let botonCrearPersona = document.querySelector("#btnCrearPersona");
 
 botonCrearPersona.addEventListener("click", crearPersona);
-function crearPersona() {
+function crearPersona(e) {
+  e.preventDefault();
   const nombre = document.getElementById("nombre").value;
   const edad = parseInt(document.getElementById("edad").value);
   const dni = document.getElementById("dni").value;
   const sexo = document.querySelector('input[name="sexo"]:checked').value;
   const peso = parseFloat(document.getElementById("peso").value);
   const altura = parseFloat(document.getElementById("altura").value);
-  const anioNacimiento = parseInt(
-    document.getElementById("anio-nacimiento").value
-  );
+  const anioNacimiento = parseInt(document.getElementById("anio-nacimiento").value);
 
-  persona = new Persona(nombre, edad, dni, sexo, peso, altura, anioNacimiento);
-
-  console.log(botonCrearPersona);
-
-  return persona;
+  let persona = new Persona(
+    nombre,
+    edad,
+    dni,
+    sexo,
+    peso,
+    altura,
+    anioNacimiento);
+    console.log(persona);
+alert(`Se cargó la información de ${nombre}
+edad: ${edad},
+dni: ${dni},
+sexo: ${sexo},
+peso: ${peso},
+altura: ${altura}
+año de nacimiento: ${anioNacimiento}`)
+formulario.reset();
 }
+
 let botonEsMayorDeEdad = document.querySelector("#btnMayorEdad");
 botonEsMayorDeEdad.addEventListener("click", esMayorDeEdad);
 function esMayorDeEdad() {
@@ -41,9 +53,39 @@ function esMayorDeEdad() {
 }
 
 let botonMostrarGeneracion = document.querySelector("#btnMostrarGeneracion");
-botonMostrarGeneracion.addEventListener("click", function () {
-  alert(persona.mostrarGeneracion());
-});
+botonMostrarGeneracion.addEventListener("click", mostrarGeneracion);
+function mostrarGeneracion() {
+  let generacion;
+  let rasgoCaracteristico;
+
+  const nombre = document.getElementById("nombre").value;
+  const anioNacimiento = parseInt(
+    document.getElementById("anio-nacimiento").value
+  );
+
+  if (anioNacimiento >= 1994 && anioNacimiento <= 2010) {
+    generacion = "Generación Z";
+    rasgoCaracteristico = "Irreverencia";
+  } else if (anioNacimiento >= 1981 && anioNacimiento <= 1993) {
+    generacion = "Generación Y (Millennials)";
+    rasgoCaracteristico = "Frustración";
+  } else if (anioNacimiento >= 1969 && anioNacimiento <= 1980) {
+    generacion = "Generación X";
+    rasgoCaracteristico = "Obsesión con el éxito";
+  } else if (anioNacimiento >= 1949 && anioNacimiento <= 1968) {
+    generacion = "Baby Boom";
+    rasgoCaracteristico = "Ambición";
+  } else if (anioNacimiento >= 1930 && anioNacimiento <= 1948) {
+    generacion = "Silent Generation (Los niños de la postguerra)";
+    rasgoCaracteristico = "Austeridad";
+  } else {
+    generacion = "No se pudo determinar la generación";
+    rasgoCaracteristico = "Desconocido";
+  }
+  alert(
+    `${nombre} pertenece a la ${generacion}, cuyo rasgo característico es ${rasgoCaracteristico}`);
+}
+
 class Persona {
   #nombre;
   #edad;
@@ -59,7 +101,7 @@ class Persona {
     this.#peso = peso;
     this.#altura = altura;
     this.#anioNacimiento = anioNacimiento;
-    this.#dni = this.generaDNI();
+    this.#dni = dni;
   }
 
   get nombre() {
@@ -117,45 +159,6 @@ class Persona {
   set dni(nuevoDNI) {
     this.#dni = nuevoDNI;
   }
-
-  mostrarGeneracion(persona) {
-    let generacion;
-    let rasgoCaracteristico;
-
-    if (this.#anioNacimiento >= 1994 && this.#anioNacimiento <= 2010) {
-      generacion = "Generación Z";
-      rasgoCaracteristico = "Irreverencia";
-    } else if (this.#anioNacimiento >= 1981 && this.#anioNacimiento <= 1993) {
-      generacion = "Generación Y (Millennials)";
-      rasgoCaracteristico = "Frustración";
-    } else if (this.#anioNacimiento >= 1969 && this.#anioNacimiento <= 1980) {
-      generacion = "Generación X";
-      rasgoCaracteristico = "Obsesión con el éxito";
-    } else if (this.#anioNacimiento >= 1949 && this.#anioNacimiento <= 1968) {
-      generacion = "Baby Boom";
-      rasgoCaracteristico = "Ambición";
-    } else if (this.#anioNacimiento >= 1930 && this.#anioNacimiento <= 1948) {
-      generacion = "Silent Generation (Los niños de la postguerra)";
-      rasgoCaracteristico = "Austeridad";
-    } else {
-      generacion = "No se pudo determinar la generación";
-      rasgoCaracteristico = "Desconocido";
-    }
-
-    alert(
-      `${
-        this.#nombre
-      } pertenece a la ${generacion}, cuyo rasgo característico es ${rasgoCaracteristico}.<br>`
-    );
-  }
-
-  esMayorDeEdad() {
-    if (this.#edad >= 18) {
-      alert(`${this.#nombre} es mayor de edad.`);
-    } else {
-      alert(`${this.#nombre} no es mayor de edad.`);
-    }
-  }
 }
-//   document.querySelector('form').reset();
 
+//   document.querySelector('form').reset();
